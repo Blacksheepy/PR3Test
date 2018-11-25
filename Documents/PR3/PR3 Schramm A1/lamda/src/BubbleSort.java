e762bddf2635bbe61aefa7a2d57cf381db177614
Bianca <namuu@web.de>
2018-11-25 14:12:47 +0100

    Sortin Algorithms added

---
+++ Documents/PR3/PR3 Schramm A1/lamda/src/BubbleSort.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+ class BubbleSort<A extends Comparable<A>> implements Sort<A> {
+
+    public void sort(A arr[]) {
+        A temp;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    System.out.println(arr[j] + " compare to " + arr[j + 1]);
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
