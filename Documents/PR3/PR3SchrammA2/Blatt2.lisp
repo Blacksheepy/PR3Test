ec93d4433fbecbc01d26946dc64a59b9ba15378a
wifi-cable <Wifi-cable@users.noreply.github.com>
2018-12-13 12:38:53 +0100

    ceated outline 'blatt2' empty methods and comments like a java interface

---
+++ Documents/PR3/PR3SchrammA2/Blatt2.lisp
@@ -0,0 +1,25 @@
+;Aufgabe 1a liste umkehren
+(defun my-reverse(list )
+;code aus vorlesung
+)
+
+;Aufgabe 1b Geschachtelte Listen umkehren
+(defun my-reverseR(list )
+;code aus forlesung
+)
+
+; Aufgabe 1c Elemente tauschen
+(defun rotiere(list )
+)
+
+; Aufgabe 1d Element einfügen
+
+(defun neues-vorletztes( lis)
+ )
+
+;Aufgabe 1e Länge einer Liste berechnen
+(defun my-length(list)
+)
+; Aufgabe 1f Länge einer geschachtelten Liste berechnen
+(defun my-lengthR(list)
+)
